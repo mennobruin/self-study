94e69c014d9a5e8f9e685b57fd7a56bdfc4365ed
MennoBruin <mennombruin@gmail.com>
2023-07-25 20:30:42 +0200

    Added write to dbconnection

--- python/web-scraping/code/database/db_connection.py
+++ python/web-scraping/code/database/db_connection.py
@@ -4,16 +4,32 @@ import sqlite3
 class DBConnection:
 
     DB_NAME = '..\\..\\resources\\data\\nos_data.db'
+    INSERT_QUERY = 'INSERT INTO {0} ({1}) VALUES ({2})'
 
     def __init__(self):
-        self.connection = None
+        self.connection: sqlite3.Connection = None
+        self.cursor: sqlite3.Cursor = None
 
     def __enter__(self):
         self.connection = sqlite3.connect(self.DB_NAME)
+        self.cursor = self.connection.cursor()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.commit()
         self.connection.close()
+
+    def write(self, db_name, db_cols, data):
+        cols = ""
+        values = ""
+
+        for col in db_cols:
+            cols += f"{col}, "
+            values += "?, "
+
+        query = self.INSERT_QUERY.format(db_name, cols, values)
+        print(query)
+
+        self.cursor.executemany(query, data)
 
 
 if __name__ == '__main__':
